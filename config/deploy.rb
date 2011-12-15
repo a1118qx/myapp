025d5cbe867151b2bee69ac8350aa763773b706f
qiyue xi <a1118qx@aiit.ac.jp>
2011-12-15 05:02:29 -0800

    Deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,13 +1,24 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+set :application, "myapp"
+set :deploy_to, "/home/rails/myapp"
 
-set :scm, :subversion
+set :repository,  "git@github.com:a1118qx/myapp"
+set :scm, :git
+
+set :use_sudo, false
+
+role :web, "localhost"
+role :app, "localhost"
+role :db,  "localhost", :primary => true
+
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
+
 # Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
-
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
 
 # if you're still using the script/reaper helper you will need
 # these http://github.com/rails/irs_process_scripts
